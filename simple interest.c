e312564749259227db76e439acaaf2972c394f3b
Yashasvi1202 <vanani.yashu25@gmail.com>
2025-08-15 19:44:16 +0530

    simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+  float principal, rate, time, simpleInterest;
+
+    // Input values
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of interest (in %%): ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+
+    // Calculate Simple Interest
+    simpleInterest = (principal * rate * time) / 100;
+
+    // Output the result
+    printf("Simple Interest = %.2f\n", simpleInterest);
+
+    return 0;
+}
